38509e45529802ccc57b74a360f6bef28e2e12bc
nehalchoraria <nehalchoraria09@gmail.com>
2019-01-24 18:13:55 +0530

    update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -122,6 +122,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
