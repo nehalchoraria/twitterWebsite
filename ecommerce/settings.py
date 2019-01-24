93c4aa1c38fcc2a3e2d40e4341965707a9813975
nehalchoraria <nehalchoraria09@gmail.com>
2019-01-24 18:09:53 +0530

    v3

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x)iv19+ocum5w=b8$fj_%bovsvm%u%@9wj@14ke$hor_c)k6#w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
